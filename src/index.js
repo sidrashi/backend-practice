63b61c73f11c4d228498c28fd57218c396667dc6
Siddharth Rashiwala <srashiwala@yahoo.in>
2025-10-10 21:46:10 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERRR:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 6000, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !!!", err);
+  });
 
 /*
 import mongoose from "mongoose";
@@ -26,7 +39,7 @@ const app = express()
     });
   } catch (error) {
     console.error("ERROR: ", error);
-    throw err;
+    throw error;
   }
 })();
 */
